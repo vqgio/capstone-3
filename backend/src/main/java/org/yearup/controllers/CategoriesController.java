b1c0c671cf87be49a681bc5b833cf47a2d4cf182
Gio <giovannizavala82@gmail.com>
2025-06-24 09:48:39 -0500

    Completed all annotations for CategoriesController.

--- backend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -59,6 +60,8 @@ public class CategoriesController
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping
+    @Secured("ROLE_ADMIN")
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
@@ -67,6 +70,8 @@ public class CategoriesController
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("/{id}")
+    @Secured("ROLE_ADMIN")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
@@ -75,6 +80,7 @@ public class CategoriesController
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable int id)
     {
         // delete the category by id
