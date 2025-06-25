671cacadbce53b3cbee8699103d80d0c7d77c879
Gio <giovannizavala82@gmail.com>
2025-06-25 15:45:10 -0500

    properly filters now, BUG #1 FIXED

--- backend/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ backend/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -22,7 +22,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color)
     {
         List<Product> products = new ArrayList<>();
-
+        //max price functions properly.
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
                 "   AND (price >= ? OR ? = -1) " +
