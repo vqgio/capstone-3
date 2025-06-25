971fb726ec6a5c17b9e5d61fc9fd0d84a98de849
Gio <giovannizavala82@gmail.com>
2025-06-25 15:44:49 -0500

    trying to find min and max price values bc app shows both as min.

--- frontend/js/application.js
+++ frontend/js/application.js
@@ -105,7 +105,7 @@ function setMinPrice(control)
 
 function setMaxPrice(control)
 {
-    // const slider = document.getElementById("min-price");
+    // const slider = document.getElementById("max-price");
     const label = document.getElementById("max-price-display")
     label.innerText = control.value;
 
